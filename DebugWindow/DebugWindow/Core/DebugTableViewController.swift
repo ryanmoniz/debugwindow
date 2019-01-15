581d4578e9f61a72ec9a0216156507c3e36e21b4
Ryan Moniz <ryanmoniz@gmail.com>
2019-01-15 09:05:39 -0800

    Add files via upload
    initial commit of debug window project

---
+++ DebugWindow/DebugWindow/Core/DebugTableViewController.swift
@@ -0,0 +1,97 @@
+//
+//  DebugTableViewController.swift
+//  DebugWindow
+//
+//  Created by Ryan Moniz on 1/2/19.
+//  Copyright © 2019 Ryan Moniz. All rights reserved.
+//
+
+import UIKit
+
+class DebugTableViewController: UITableViewController {
+
+    @IBAction func doneAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SetSGIdentifier", for: indexPath)
+            return cell
+        } else if indexPath.row == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SendHypoIdentifier", for: indexPath)
+            return cell
+        } else if indexPath.row == 2 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SendFeedbackIdentifier", for: indexPath)
+            return cell
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmptyIdentifier", for: indexPath)
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DebugWindow/DebugWindow/Core/DebugWIndow.swift
@@ -0,0 +1,56 @@
+//
+//  DebugWIndow.swift
+//  DebugWindow
+//
+//  Created by Ryan Moniz on 1/2/19.
+//  Copyright © 2019 Ryan Moniz. All rights reserved.
+//
+
+import UIKit
+
+public class DebugWindow {
+    public static let sharedInstance = DebugWindow()
+    
+    private lazy var slideInTransitioningDelegate = SlideInPresentationManager()
+    private var debugVC: DebugTableViewController!
+    
+    //MARK: - Methods
+    public func setup() {
+        NSLog("DebugWindow init")
+        
+        //configure swipe action
+        let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipedScreen))
+        swipeGesture.direction = UISwipeGestureRecognizer.Direction.right
+        swipeGesture.numberOfTouchesRequired = 3
+        
+        if let window = UIApplication.shared.delegate?.window {
+            window?.addGestureRecognizer(swipeGesture)
+        } else {
+            NSLog("ERROR: app has no window defined in AppDelegate????")
+        }
+    }
+    
+    @objc private func swipedScreen(swipeGesture:UISwipeGestureRecognizer) {
+        NSLog("swipedScreen called")
+        
+        let sb = UIStoryboard(name: "DebugTableViewController", bundle: nil)
+        
+        guard let debugNavVC = sb.instantiateInitialViewController() as? UINavigationController else {
+            NSLog("ERROR: missing DebugTableViewController storyboard/initalizer")
+            return
+        }
+        
+        //configure uipresentationcontroller
+        slideInTransitioningDelegate.direction = .left
+        slideInTransitioningDelegate.disableCompactHeight = false
+        debugNavVC.transitioningDelegate = slideInTransitioningDelegate
+        debugNavVC.modalPresentationStyle = .custom
+        
+        //present debug view
+        if let window = UIApplication.shared.delegate?.window {
+            window?.rootViewController?.present(debugNavVC, animated: true, completion: nil)
+        } else {
+            NSLog("ERROR: app has no window defined in AppDelegate????")
+        }
+    }
+}
